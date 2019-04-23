ab3920850d5a3c2c20b6c1f25b35922c8680f0ee
Graham Heffern <gheffern@gmail.com>
2019-04-23 05:36:49 -0500

    cleaned up the script a little bit, also changed how I handle 0.0.0.0

--- PathPingVis.ps1
+++ PathPingVis.ps1
@@ -15,7 +15,7 @@ function printTree($tree) {
 
 function isChild($ip, $children) {
     foreach ($child in $children) {
-        if ($ip -eq $child.ip) {
+        if ($ip -eq $child.ip -and $ip -ne '0.0.0.0') {
             $script:foundChild = $child
             return $true;
         }
@@ -28,34 +28,41 @@ function makeNode($ip, $domain) {
         $nodeHostName = (Resolve-DnsName $ip -ErrorAction silentlycontinue -QuickTimeout ).NameHost
     }
     else {
-        $nodeHostName = "$domain path, unknown host " + (get-randomString)
+        $nodeHostName = "unknown_host " + (get-randomString)
     }
     New-Object -TypeName psobject -Property @{hostname = $nodeHostName; ip = $ip; children = @() }
 }
+function createTree(){
+    $domains = Get-Content .\top_domains.txt
+    $global:rootNode = New-Object -TypeName psobject -Property @{hostname = "root node"; ip = "N/A"; children = @() }
 
-$domains = Get-Content .\top_10_domains.txt
-$global:rootNode = New-Object -TypeName psobject -Property @{hostname = "root node"; ip = "N/A"; children = @() }
-
-foreach ($domain in $domains) {
-    $currentNodeTree = $global:rootNode 
-    $previousNodeTree = $global:rootNode
-    Write-Host "Working Domain $domain"
-    $result = Test-NetConnection $domain -TraceRoute
-    $route = $result.TraceRoute
-    foreach ($ip in $route) {
-        if (isChild $ip $previousNodeTree.children) {
-            write-host "`t is child is true"
-            $currentNodeTree = $script:foundChild 
+    foreach ($domain in $domains) {
+        $currentNodeTree = $global:rootNode 
+        $previousNodeTree = $global:rootNode
+        Write-Host "Getting route for domain: $domain"
+        $result = Test-NetConnection $domain -TraceRoute
+        $route = $result.TraceRoute
+        foreach ($ip in $route) {
+            if (isChild $ip $previousNodeTree.children) {
+                $currentNodeTree = $script:foundChild 
+            }
+            else {
+                $currentNode = makeNode $ip $domain
+                $currentNodeTree.children += $currentNode
+                $currentNodeTree = $currentNode
+            }
+            write-host ("`t" + $currentNodeTree.ip + ' ' + $currentNodeTree.hostname)
+            $previousNodeTree = $currentNodeTree
         }
-        else {
-            $currentNode = makeNode $ip $domain
-            $currentNodeTree.children += $currentNode
-            $currentNodeTree = $currentNode
-        }
-        write-host ("`t" + $currentNodeTree.ip + ' ' + $currentNodeTree.hostname)
-        $previousNodeTree = $currentNodeTree
+        $leaf = New-Object -TypeName psobject -Property @{hostname = $domain; ip = "N/A"; children = @() }
+        $currentNodeTree.children += $leaf 
     }
-    $leaf = New-Object -TypeName psobject -Property @{hostname = $domain; ip = "N/A"; children = @() }
-    $currentNodeTree.children += $leaf 
 }
 
+function main(){
+    createTree
+    "digraph G {" | out-file -encoding ASCII .\graph.dot
+    printTree $rootNode | out-file -encoding ASCII -Append .\graph.dot
+    "}" | out-file -encoding ASCII -Append .\graph.dot
+    & "C:\Program Files (x86)\Graphviz2.38\bin\dot.exe" -Tsvg -o graph.svg .\graph.dot 
+}
